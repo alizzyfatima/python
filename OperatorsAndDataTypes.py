77765307f44f753aeeab2e083d66e3393b73e0ea
alizzyfatima <maliksami200283@gmail.com>
2025-05-17 20:59:45 +0200

    Create OperatorsAndDataTypes.py

---
+++ OperatorsAndDataTypes.py
@@ -0,0 +1,32 @@
+x=20.76
+print(x)
+print(type(x))
+#here i change the data type into integer
+y=int(x)
+#this is integer
+print(y)
+print(type(y))
+num1=43
+num2=3
+print("number 1", num1)
+print("number 2", num2)
+print("addition:", num1+num2)
+print("differnce:", num1-num2)
+print("product:", num1*num2)
+print("division:", num1/num2)
+print("floor division:", num1//num2)
+print("modulus operation:", num1%num2)
+print("square:", num2**num2)
+print("square root:", num1**0.5)
+print("equal?:", num1==num2)
+print("number 1 greater?:", num1>num2)
+print("number 2 greater?:", num1<num2)
+print("not equal?:", num1!=num2)
+result = num1/2+num2**2+10
+print("result of given equation is:",result)
+name="alizy fatima"
+print(name)
+print(name[0:5])
+print(name[6:12])
+print(name[0:12:2])
+
