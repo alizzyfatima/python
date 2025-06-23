8ca6db67986ebb7a60bea651cbe52a91424085aa
alizzyfatima <maliksami200283@gmail.com>
2025-06-23 21:15:49 +0200

    Create introduceYourself.py

---
+++ introduceYourself.py
@@ -0,0 +1,12 @@
+def introduction(name,age):
+    print("hello I am",name)
+    print("I am", age)
+introduction(" humayrah",  25)
+introduction( "alizy" , 14)
+
+def introduction(name,age):
+    print("hello I am",name)
+    print("I am", age , " years old i am alizys smaller brother")
+introduction("Amaan", 9)
+
+
