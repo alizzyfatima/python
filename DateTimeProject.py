ff8094a6366a9df8f3ded6901cba0f9405ccdc0a
alizzyfatima <maliksami200283@gmail.com>
2025-06-29 14:55:09 +0200

    Create DateTimeProject.py

---
+++ DateTimeProject.py
@@ -0,0 +1,9 @@
+#using now() to get current time 
+current_time=datetime.datetime.now()
+
+#printing value of now.
+print("Time now at greenwhich meridian is : ", end = "")
+print (current_time)
+
+#print calendar of year 2025
+print("\n", calendar.calendar(2025))
